69991bc64eea8f1ad40e82d792b54d33b4f7502e
Jxck <block.rxckin.beats@gmail.com>
2013-03-22 14:34:47 +0900

    add handleSynStreamFrame()

--- handler.go
+++ handler.go
@@ -7,6 +7,49 @@ import (
 	"net/http"
 )
 
+func handleSynStreamFrame(framer *spdy.Framer, synStream *spdy.SynStreamFrame) error {
+	debug("recv %v", synStream)
+
+	// debug data
+	var ResponseData = "<h1>hello</h1>"
+
+	var HeadersFixture = http.Header{
+		":version":       []string{"http/1.1"},
+		":status":        []string{"200 OK"},
+		"location":       []string{"http://localhost:3000/"},
+		"content-type":   []string{"text/html; charset=utf-8"},
+		"content-length": []string{"14"},
+		"server":         []string{"speedy"},
+	}
+
+	// send reply
+	synReply := spdy.SynReplyFrame{
+		CFHeader: spdy.ControlFrameHeader{}, //Flag is 0x00
+		StreamId: synStream.StreamId,
+		Headers:  HeadersFixture,
+	}
+
+	err := framer.WriteFrame(&synReply)
+	if err != nil {
+		return err
+	}
+	debug("send %v", &synReply)
+
+	// send data
+	dataFrame := spdy.DataFrame{
+		StreamId: synStream.StreamId,
+		Flags:    spdy.DataFlagFin,
+		Data:     []byte(ResponseData),
+	}
+
+	err = framer.WriteFrame(&dataFrame)
+	if err != nil {
+		return err
+	}
+	debug("send %v", &dataFrame)
+	return nil
+}
+
 func handleConnection(conn net.Conn) error {
 	framer, err := spdy.NewFramer(conn, conn)
 	if err != nil {
@@ -21,47 +64,10 @@ func handleConnection(conn net.Conn) error {
 
 		switch frametype := frame.(type) {
 		case *spdy.SynStreamFrame:
-			synStream := frametype //frame.(*spdy.SynStreamFrame)
-			debug("recv %v", synStream)
-
-			// debug data
-			var ResponseData = "<h1>hello</h1>"
-
-			var HeadersFixture = http.Header{
-				":version":       []string{"http/1.1"},
-				":status":        []string{"200 OK"},
-				"location":       []string{"http://localhost:3000/"},
-				"content-type":   []string{"text/html; charset=utf-8"},
-				"content-length": []string{"14"},
-				"server":         []string{"speedy"},
-			}
-
-			// send reply
-			synReply := spdy.SynReplyFrame{
-				CFHeader: spdy.ControlFrameHeader{}, //Flag is 0x00
-				StreamId: synStream.StreamId,
-				Headers:  HeadersFixture,
-			}
-
-			err := framer.WriteFrame(&synReply)
+			err := handleSynStreamFrame(framer, frametype)
 			if err != nil {
 				return err
 			}
-			debug("send %v", &synReply)
-
-			// send data
-			dataFrame := spdy.DataFrame{
-				StreamId: synStream.StreamId,
-				Flags:    spdy.DataFlagFin,
-				Data:     []byte(ResponseData),
-			}
-
-			err = framer.WriteFrame(&dataFrame)
-			if err != nil {
-				return err
-			}
-			debug("send %v", &dataFrame)
-
 		default:
 			debug("unknown frame")
 			log.Fatalf("unknown frame %v", frametype)
